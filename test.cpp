fc838e8dd29e2e5951e5318e659201b998714387
Marcos Rodríguez <marcos@unizar.es>
2018-06-29 13:50:49 +0200

    something wrong...

--- test.cpp
+++ test.cpp
@@ -11,9 +11,10 @@ using namespace std;
 enum _header {TASK_DONE, GIVE_ME, NO_MORE, GIVE_YOU};
 
 
-const double par[] = {2.0, 3.0};
-int N = 1001;
-int nvar=27;
+const double par0[] = {2.0, 3.0};
+const double par1[] = {-3.0, -2.0};
+int M = 4;
+int N = 3;
 
 
 
@@ -26,19 +27,21 @@ inline int min(int a, int b) {
 typedef struct _Task {
   int header;           // COMMUNICATION PROTOCOL
   int index;
-  double par;           // INPUT
-  double x0[27];        // INPUT
+  int i;
+  int j;
+  double par0;           // INPUT
+  double par1;           // INPUT
 
-  double xf[27];        // OUTPUT
   int latidos;
   int picos;
+  double xf[27];
 } Task;
 
 
 
 
 ostream & operator<<(ostream &output, const struct _Task &op) {
-  output << "(" << op.latidos << ", " << op.picos << ") " << op.xf[0] << " " << op.xf[nvar-1] << " <---> par = " << op.par;
+  output << op.index << " " << op.i << " " << op.j << " (" << op.latidos << ", " << op.picos << ") " << op.xf[0] << " " << op.xf[26] << " <---> par = " << op.par0 << ", " << op.par1;
 
   return output;
 }
@@ -71,25 +74,27 @@ int main(int argc, char *argv[]) {
 
   if (proc_id == 0) {
     int activeWorkers = world_size-1;
-    int taskSize = N;
-    int pendingTask = N;
+    int taskSize = M*N;
+    int pendingTask = M*N;
     int completedTask = 0;
-    rop.resize(N);
+    rop.resize(taskSize);
 
     // INITIALIZE TASK INPUT
-    for(int i=0; i<N; ++i) {
-      rop[i].index = i;
-      rop[i].par = par[0] + i*((par[1]-par[0])/(N-1.0));
-      for(int j=0; j<nvar; ++j)
-        rop[i].x0[j] = rop[i].xf[j] = -1.0;
-      rop[i].latidos = -1;
-      rop[i].picos = -1;
+    for(int i=0; i<M; ++i) for(int j=0; j<N; ++j) {
+      rop[j*M+i].i = i;
+      rop[j*M+i].j = j;
+      rop[j*M+i].index = j*M+i;
+      rop[j*M+i].par0 = par0[0] + i*((par0[1]-par0[0])/(M-1.0));
+      rop[j*M+i].par1 = par1[0] + j*((par1[1]-par1[0])/(N-1.0));
+
+      rop[j*M+i].latidos = -1;
+      rop[j*M+i].picos = -1;
     }
 
     // Task pendingTask(taskSize), completedTask(taskSize);
 
 
-    while(!completedTask || activeWorkers) {
+    while(activeWorkers) {
       for(int worker=1; worker < world_size; ++worker) {
         MPI_Iprobe(worker, 0, MPI_COMM_WORLD, &incomingMessage,
                 &status);
@@ -100,60 +105,87 @@ int main(int argc, char *argv[]) {
                 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
           switch (buffer.header) {
             case TASK_DONE:
-              cout << "\t\tworker " << worker << " has DONE A TASK" << endl;
-
+              cout << "\t\tworker " << worker << " has DONE A TASK " << buffer.index << endl;
               // UPLOAD DATA
               rop[buffer.index] = buffer;
+              completedTask += 1;
               // END UPLOAD
               cout << "\t\tcompleted = " << completedTask << endl;
+              cout << "\t\taskSize = " << taskSize << endl;
               cout << "\t\tcriterio = " << (completedTask != taskSize)
                       << endl;
-              ++completedTask;
               break;
             case GIVE_ME:
               cout << "\t\tworker " << worker << " asks for TASK" << endl;
-              if(pendingTask){
+              if(pendingTask>0){
                 cout << "\t\tso send task to woker " << worker << endl;
-                int index = taskSize-pendingTask;
-                buffer = rop[index];
-                buffer.header = GIVE_YOU;
-                pendingTask -= 1;
+                for(int i=0; i<M; ++i) {
+                  int index = taskSize-pendingTask;
+                  buffer = rop[index];
+                  buffer.header = GIVE_YOU;
+                  --pendingTask;
+                  MPI_Send(&buffer, sizeof(Task), MPI_CHAR, worker,
+                    0, MPI_COMM_WORLD);
+                }
 
               } else {
                 cout << "\t\tbut no more tasks left for worker " << worker << endl;
                 buffer.header = NO_MORE;
                 --activeWorkers;
                 cout << "\t\t\t\tstill active = " << activeWorkers << endl;
+                MPI_Send(&buffer, sizeof(Task), MPI_CHAR, worker,
+                  0, MPI_COMM_WORLD);
               }
-              MPI_Send(&buffer, sizeof(Task), MPI_CHAR, worker,
-                      0, MPI_COMM_WORLD);
           }
         }
       }
     }
   } else {
     while(1) {
-      buffer.header = GIVE_ME;
-      MPI_Send(&buffer, sizeof(Task), MPI_CHAR, 0,
+      vector<Task> lineBuffer(M);
+
+      lineBuffer[0].header = GIVE_ME;
+      MPI_Send(&lineBuffer[0], sizeof(Task), MPI_CHAR, 0,
               0, MPI_COMM_WORLD);
-      MPI_Recv(&buffer, sizeof(Task), MPI_CHAR, 0,
+      MPI_Recv(&lineBuffer[0], sizeof(Task), MPI_CHAR, 0,
               0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-      if(buffer.header == NO_MORE) {
+      if(lineBuffer[0].header == NO_MORE) {
         cout << "\t\t\t\t\t\tworker " << processor_name << ", " << proc_id << " finished job" << endl;
         break;
       }
+      // HAY MAS TAREAS
+      for(int i=1; i<M; ++i) {
+        MPI_Recv(&lineBuffer[i], sizeof(Task), MPI_CHAR, 0,
+        0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        lineBuffer[i] = buffer;
+      }
+
+
+      cout << "buffer 0 pars = " << lineBuffer[0].par0 << ", " << lineBuffer[0].par1 << endl;
 
       // DO THINGS //
       // LLAMAR AL INTEGRADOR
-      for(int j=0; j<nvar; ++j)
-        buffer.xf[j] = buffer.par;
-      buffer.latidos = 2;
-      buffer.picos = 30000 + buffer.index;
 
+      // for(int j=0; j<27; ++j)
+      //   lineBuffer[0].xf[j] = 10.0;
+      lineBuffer[0].latidos = 2;
+      lineBuffer[0].picos = 30000 + buffer.index;
+      lineBuffer[0].header = TASK_DONE;
+      //
+      // std::cout << "\t\t pp = " << lineBuffer[0].par0 << std::endl;
+      //
+      for(int i=1; i<M; ++i) {
+      //   for(int j=0; j<27; ++j)
+      //     lineBuffer[i].xf[j] = lineBuffer[i-1].xf[j] + 10.0;
+        lineBuffer[i].latidos = 2;
+        lineBuffer[i].picos = 30000 + buffer.index;
+        lineBuffer[i].header = TASK_DONE;
+      }
 
-      buffer.header = TASK_DONE;
-      //buffer[1] = 1;
-      MPI_Send(&buffer, sizeof(Task), MPI_CHAR, 0,
+      cout << "buffer 0 pars = " << lineBuffer[0].par0 << ", " << lineBuffer[0].par1 << endl;
+
+      for(int i=0; i<M; ++i)
+        MPI_Send(&lineBuffer[i], sizeof(Task), MPI_CHAR, 0,
               0, MPI_COMM_WORLD);
 
     }
@@ -161,7 +193,7 @@ int main(int argc, char *argv[]) {
 
   usleep(1000);
   if(proc_id == 0) {
-    for(int i=0; i<N; ++i)
+    for(int i=0; i<M*N; ++i)
       cout << rop[i] << endl;
   }
 
